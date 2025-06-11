25fd3611f16311a037a99b25d2f38396721f82a3
David Jennings <davidjjennings99@gmail.com>
2025-06-10 22:07:20 -0400

    FIX PRISMA

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -1,5 +1,3 @@
-import { PrismaClient } from "@prisma/client/edge"
-
-console.log('DB URL:', process.env.DATABASE_URL)
+import { PrismaClient } from "@prisma/client"
  
 export const prisma = new PrismaClient()
