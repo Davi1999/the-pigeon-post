19a2229bf782b822f43485c753c9c4882219d9c1
David Jennings <davidjjennings99@gmail.com>
2026-03-16 09:36:25 -0400

    add dark mode for special edition image

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -93,7 +93,7 @@ export default async function DashboardPage() {
           <img
             src="/extra-extra.svg"
             alt="Read the latest updates"
-            className="h-auto w-full"
+            className="h-auto w-full extra-extra-img"
           />
           <p className="mt-2 text-center text-sm font-notable uppercase tracking-wide">
             HOT OFF THE PRESS!
